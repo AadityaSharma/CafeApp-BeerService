6f4313d52a1fbcba36405cd2a8e87986be068f21
Aaditya Sharma <accolite-asharma@evernote.com>
2020-10-22 00:56:49 +0530

    Initial project add. Closes #1

---
+++ src/main/java/aaditya/dev/cafeappbeerservice/CafeappBeerserviceApplication.java
@@ -0,0 +1,13 @@
+package aaditya.dev.cafeappbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeappBeerserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeappBeerserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/aaditya/dev/cafeappbeerservice/CafeappBeerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package aaditya.dev.cafeappbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeappBeerserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
