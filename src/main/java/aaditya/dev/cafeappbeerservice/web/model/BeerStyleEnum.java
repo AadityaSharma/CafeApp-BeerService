a7bf7ff3ba36a5e4a468963cf2c67c3e9f7645f9
Aaditya Sharma <accolite-asharma@evernote.com>
2020-10-22 01:18:20 +0530

    Adding web data model. Closes #2

---
+++ src/main/java/aaditya/dev/cafeappbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,14 @@
+package aaditya.dev.cafeappbeerservice.web.model;
+
+public enum BeerStyleEnum {
+    LAGER,
+    PISLSNER,
+    STOUT,
+    GOSE,
+    PORTER,
+    ALE,
+    WHEAT,
+    IPA,
+    PALE_ALE,
+    SAISON
+}
